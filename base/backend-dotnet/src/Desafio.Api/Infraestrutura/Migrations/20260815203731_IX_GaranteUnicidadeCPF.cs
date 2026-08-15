5e0e7a34b82622e3999faa21a785526d3a5c45dd
Paulo Andrade <parthur207@gmail.com>
2026-08-15 17:41:57 -0300

    FIX (beneficiario): Migrations para inserção da coluna "ExcluidoEm" e implemento de indice de CPF.

---
+++ base/backend-dotnet/src/Desafio.Api/Infraestrutura/Migrations/20260815203731_IX_GaranteUnicidadeCPF.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Desafio.Api.Infraestrutura.Migrations
+{
+    /// <inheritdoc />
+    public partial class IX_GaranteUnicidadeCPF : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Beneficiarios_Cpf",
+                table: "Beneficiarios",
+                column: "Cpf",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Beneficiarios_Cpf",
+                table: "Beneficiarios");
+        }
+    }
+
+}
